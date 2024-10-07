97d7314718a212fffab05e03ec0d863361f76260
SuyogLuitel <suyogluitel235@gmail.com>
2024-10-07 23:30:38 +0545

    "feat: collaborative editor room"

---
+++ app/Provider.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+import Loader from "@/components/Loader";
+import { getClerkUser } from "@/lib/actions/user.actions";
+
+const Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <LiveblocksProvider
+      authEndpoint={"/api/liveblocks-auth"}
+      resolveUsers={async ({ userIds }) => {
+        const users = await getClerkUser({ userIds });
+
+        return users;
+      }}
+    >
+      <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
